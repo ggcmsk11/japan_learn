939cf40c338b24358e2fbcdc2683a830d3562c70
ggcmsk11 <Evan1895V76XM@hotmail.com>
2025-06-01 14:11:38 +0800

    Add missing onMounted import

--- src/views/GrammarPage.vue
+++ src/views/GrammarPage.vue
@@ -70,7 +70,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 import { ElMessage } from 'element-plus'
